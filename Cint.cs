f026f09fc8f15c71372bcda00fb71b2342a16d6f
nmcasasr <38595223+nmcasasr@users.noreply.github.com>
2018-09-16 14:49:03 -0500

    sfsdf

---
+++ Cint.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 1;
+            int tem = 0;
+            int contador = 0;
+            Console.Write("Escribe un numero: ");
+            String nString = Console.ReadLine();
+            int n = Int32.Parse(nString);
+
+            while (contador < n)
+            {
+                contador = contador + 1;
+                tem = y;
+                y = y + x;
+                x = tem;
+                if(y < 0)
+                {
+                Console.WriteLine(y);
+                    break;
+                }
+                
+            }
+        }
+    }
+}
